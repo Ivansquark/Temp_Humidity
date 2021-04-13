de2c38be2d81fc31701f4637526050a9ff9cb265
Ivansquark <squark@mail.ru>
2021-04-13 19:21:18 +0300

    +dht22 data

--- inc/dht22.h
+++ inc/dht22.h
@@ -12,13 +12,19 @@ public:
     static DHT22_interrupt* pThis;
      
     void setActiveIrq(void) {pThis=this;}
-private:
+    uint8_t getData();
     void init();
+    uint8_t data[5] = {0};
+    float temperature = 0;
+    float humidity = 0;
+private:
     void InterruptHandle();
     void delay_us(uint32_t us);
+    void receiving_init();
     uint32_t us_counter = 0;
     inline void pin_on() __attribute__((__always_inline__));
     inline void pin_off() __attribute__((__always_inline__));
+    inline bool isPinHigh() __attribute__((__always_inline__)) {return GPIOB->IDR & GPIO_IDR_IDR11;}
 };
 
 class DHT22_FR: public iTaskFR, DHT22_interrupt {
--- src/dht22.cpp
+++ src/dht22.cpp
@@ -4,7 +4,7 @@ DHT22_interrupt* DHT22_interrupt::pThis = nullptr;
 
 DHT22_interrupt::DHT22_interrupt() {
     pThis = this;
-    init();
+    //init();
 }
 
 void DHT22_interrupt::init() {
@@ -13,6 +13,47 @@ void DHT22_interrupt::init() {
     GPIOB->CRH |= GPIO_CRH_CNF10_0; //0:1 open Drain output
     GPIOB->CRH |= GPIO_CRH_MODE10; // max speed
     GPIOB->ODR |= GPIO_ODR_ODR10; // high level
+    receiving_init();
+}
+
+uint8_t DHT22_interrupt::getData() {
+    pin_off(); pin_on(); delay_us(100000); //start condition not always needed
+    pin_off(); delay_us(18000); pin_on();
+    uint32_t timeout = 0x1000000;
+    delay_us(39); // wait for answer
+    if(isPinHigh) {
+        return 0;
+    }
+    delay_us(80); // wait for answer
+    if(isPinHigh) {
+        return 0;
+    }
+    delay_us(80); // wait for answer
+    for(int j=0;j<5;j++) {
+        data[4-j]=0;
+        for(int i=0; i<8; i++) {
+            timeout = 1000000;
+            while (!isPinHigh || timeout--); //wait for start of bit receiving condition
+            delay_us(30);
+            if(isPinHigh) {
+                data[4-j] |= (1<<(7 - i));
+                timeout = 1000000;
+                while (isPinHigh || timeout--); //wait for end of bit
+            }            
+        }
+    }
+    temperature = (float)((*(uint16_t*)(data+1)) & 0x3FFF) / 10;
+    if((*(uint16_t*)(data+1)) & 0x8000) {
+        temperature *= -1.0;
+    }
+    humidity = (float)(*(int16_t*)(data+3)) / 10;
+    return 1;
+}
+
+void DHT22_interrupt::receiving_init() {
+    pin_off();
+    delay_us(200000);
+    pin_on();
 }
 
 void DHT22_interrupt::InterruptHandle() {
@@ -38,13 +79,14 @@ void DHT22_interrupt::pin_off() {
     GPIOB->BSRR |= GPIO_BSRR_BR10;
 }
 
-
+//! FreeRtos class
 void DHT22_FR::run() {
     InterruptSubject<TIM2_IRQn> us;
     Timer tim_us(2);
     DHT22_interrupt dht_i;
     us.setInterrupt(&dht_i);
 	us.SetVector();
+    dht_i.init();
 
     while(1) {
 
